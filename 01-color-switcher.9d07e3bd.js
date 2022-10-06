7c1687473e01add8ceed8f915b4bb8ad30f81313
Sergey-Ryabko-84 <104790315+Sergey-Ryabko-84@users.noreply.github.com>
2022-10-06 23:50:44 +0000

    Deploying to gh-pages from @ Sergey-Ryabko-84/goit-js-hw-09@3643bea5e86d268b881f34e08132d8d7eb579bdb 🚀

---
+++ 01-color-switcher.9d07e3bd.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;function n(){t.stopBtn.disabled=!0,t.startBtn.disabled=!1,clearInterval(e)}t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",n),n();
+//# sourceMappingURL=01-color-switcher.9d07e3bd.js.map
