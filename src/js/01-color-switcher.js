3643bea5e86d268b881f34e08132d8d7eb579bdb
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-10-07 02:52:19 +0300

    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+import '../css/01-color-switcher.css'
+
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+
+let intervalId = null;
+
+function onStartBtnClick () {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    startBGColorChange();
+}
+
+function onStopBtnClick () {
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+    clearInterval(intervalId);
+}
+
+function startBGColorChange () {
+    intervalId = setInterval(()=>{
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+onStopBtnClick();
 
